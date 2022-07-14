50192e6ff688a1b1bda5b036924b2383b391988b
Susbot <susbot651968498@gmail.com>
2022-07-13 22:52:53 -0600

    added super user & configured admin.py

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+# Import Models from Project
+from apis import models
+
 # Register your models here.
+
+
+# Register models
+admin.site.register(models.UserProfile)
