8facf19b17a877ce0a75dbdf45b9ba2a3452d691
Susbot <susbot651968498@gmail.com>
2022-07-13 19:30:22 -0600

    User profile manager

---
+++ apis/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+# inherit from BaseUserManager which is the default manager model from Django
+class UserProfileManager(BaseUserManager):
+    """Manager for User Profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new User profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Pass the manager class in the parenthesis
+    objects = UserProfileManager()
+
+    # Add required fields - allow functionality with Django Admin & Django Auth
+
+    # Overwriting username with email field
+    USERNAME_FIELD = 'email'
+
+    # Make the username field required
+    REQUIRED_FIELDS = ['name']
+
+    # Functions for Django to interact with Customer User Model
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    # Specify the string representation of the model - Item returned when user profile object is converted to a string
+    # Recommended for all django models
+    def __str__(self):
+        """Return string representation of User"""
+        return self.email
